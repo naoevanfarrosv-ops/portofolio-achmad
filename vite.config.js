d1de523e697b98504367a532b79c964e5f331b46
Achmad Naoevan <naoevanfarros.v@gmail.com>
2025-11-10 10:54:30 +0700

    fix path build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio-achmad/', // penting untuk GitHub Pages
+  base: '/portofolio-achmad/', // portofolio-achmad
 })
